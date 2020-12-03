2637f3e574124e4cf5f8922c22e19828b2f29369
stone <stone@163.com>
2020-12-03 21:17:44 +0800

    build

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = {
     // build时构建文件的目录 构建时传入 --no-clean 可关闭该行为
     outputDir: 'dist',
     // build时放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录
-    assetsDir: '',
+    assetsDir: './',
     // 指定生成的 index.html 的输出路径 (相对于 outputDir)。也可以是一个绝对路径。
     indexPath: 'index.html',
     // 默认在生成的静态资源文件名中包含hash以控制缓存
